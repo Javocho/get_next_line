3ad1b8d6d3c2d27a6ac31b669750f857eadfaf8d
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car13s1.42barcelona.com>
2023-07-15 15:47:19 +0200

    finished next_line now i have to do tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 11:59:27 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/15 14:53:01 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/15 15:45:51 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ static char	*read_line(int fd, char *backup)
 	int		bytes_read;
 
 	bytes_read = 1;
+	buffer = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (backup)
@@ -32,7 +33,10 @@ static char	*read_line(int fd, char *backup)
 			return (NULL);
 		if (backup)
 		{
-			buffer = ft_strjoin(buffer, backup);
+			if (buffer)
+				buffer = ft_strjoin(buffer, backup);
+			else
+				buffer = backup;
 			free(backup);
 			backup = NULL;
 		}
@@ -51,8 +55,18 @@ static char	*make_backup(char *backup, char *buffer)
 			index++;
 	}
 	backup = ft_substr(buffer, index, BUFFER_SIZE - index);
-	buffer = ft_substr(buffer, 0, index);
 	return (backup);
+}
+
+char	*extract(char *buffer) //hacerlo por referencia o algo asi??s
+{
+	int	index;
+
+	index = 0;
+	while (buffer[index] != '\n')
+		index++;
+	buffer = ft_substr(buffer, 0, index);
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
@@ -62,28 +76,29 @@ char	*get_next_line(int fd)
 
 	line = read_line(fd, backup);
 	backup = make_backup(backup, line);
+	line = extract(line);
 	return (line);
 }
 
-int main()
-{
-	char *line;
-	int fd = open("archivo.txt", O_RDONLY);  // Abre el archivo en modo lectura
-    if (fd < 0) {
-        printf("No se pudo abrir el archivo\n");
-        return 1;
-    }
+// int main()
+// {
+// 	char *line;
+// 	int fd = open("archivo.txt", O_RDONLY);  // Abre el archivo en modo lectura
+//     if (fd < 0) {
+//         printf("No se pudo abrir el archivo\n");
+//         return 1;
+//     }
 
-    // Leer y imprimir todas las líneas del archivo
-    //while ((line = get_next_line(fd)) != NULL) {
-		get_next_line(fd);
-        printf("%s\n", line);
-        //free(line);  // Libera la memoria asignada a la línea
-   // }
+//     // Leer y imprimir todas las líneas del archivo
+//     //while ((line = get_next_line(fd)) != NULL) {
+// 		line = get_next_line(fd);
+//         printf("%s\n", line);
+//         //free(line);  // Libera la memoria asignada a la línea
+//    	//}
 
-    close(fd);  // Cierra el archivo
-    return 0;
-}
+//     close(fd);  // Cierra el archivo
+//     return 0;
+// }
 
 
 
